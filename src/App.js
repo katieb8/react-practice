ec65ae19c5b07943b77af8c0627b62d3b8dc3e95
Katie Burdett <katie.burdett@ao.com>
2021-11-05 17:09:45 +0000

    got the search working with the search api

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5
 
 function App() {
     const [ movies, setMovies ] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetch(FEATURED_API)
@@ -17,12 +18,30 @@ function App() {
         });
     }, []);
 
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(SEARCH_API + searchTerm)
+            .then((res) => res.json())
+            .then((data) => {
+                setMovies(data.results);
+            });
+    };
+
+    const handleOnChange = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
     return (
         <>
         <header>
-            <input className="search" 
-            type="search" 
-            placeholder="Search for movie..."/>
+            <form onSubmit={handleOnSubmit}>
+                <input className="search" 
+                type="search" 
+                placeholder="Search for movie..."
+                value={searchTerm}
+                onChange={handleOnChange}/>
+            </form>
         </header>
         <div className="movie-container">
             {movies.length > 0 && 
